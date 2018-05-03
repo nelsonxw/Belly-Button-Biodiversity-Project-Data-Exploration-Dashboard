ec08dd9d8531a68890f5725d4f686ec5e48c654b
Nelson Wang <wx_nelson@yahoo.com>
2018-05-02 20:22:30 -0500

    added import os

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import datetime as dt
 import numpy as np
 import pandas as pd
@@ -66,5 +67,5 @@ def samples(sample):
 if __name__ == '__main__':
     app.run(debug=True)
     # Bind to PORT if defined, otherwise default to 5000.
-    # port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
