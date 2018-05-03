7419683650a553ba44da98802481b2b2f892c152
Nelson Wang <32888322+nelsonxw@users.noreply.github.com>
2018-05-02 20:31:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ def samples(sample):
 if __name__ == '__main__':
     app.run(debug=True)
     # Bind to PORT if defined, otherwise default to 5000.
-    # port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=80)
+    env_port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=env_port)
