21a99a39af28cd3d605c987c85f4f52bd5f8b43d
Nelson Wang <wx_nelson@yahoo.com>
2018-05-02 19:05:07 -0500

    updated app with bind port=5000

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ if __name__ == '__main__':
     app.run(debug=True)
     # Bind to PORT if defined, otherwise default to 5000.
     # port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
