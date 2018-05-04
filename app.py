4aae5156c1827c24c244a9141fb3101538453f78
Nelson Wang <wx_nelson@yahoo.com>
2018-05-03 22:44:56 -0500

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+# import dependencies
 import os
 import pandas as pd
-
 from flask import (
     Flask,
     render_template,
@@ -8,16 +8,11 @@ from flask import (
     request,
     redirect)
 
-#################################################
-# Flask Setup
-#################################################
+# set up Flask
 app = Flask(__name__)
 
 
-#################################################
-# Flask Routes
-#################################################
-
+# define different routes
 @app.route("/")
 def home():
     # render dashboard homepage
@@ -25,50 +20,51 @@ def home():
 
 @app.route("/names")
 def names():
+    # read data into pandas and return jsonified data
     file_name = os.path.join("datasets","belly_button_biodiversity_samples.csv")
     df = pd.read_csv(file_name)
-    # df = pd.read_csv("datasets\\belly_button_biodiversity_samples.csv")
     sample_data = df.set_index("otu_id").keys()
     return jsonify(sample_data.tolist())
 
 @app.route("/otu")
 def otu_descriptions():
+    # read data into pandas and return jsonified data
     file_name = os.path.join("datasets","belly_button_biodiversity_otu_id.csv")
     df = pd.read_csv(file_name)
-    # df = pd.read_csv("datasets\\belly_button_biodiversity_otu_id.csv")
     otu_descriptions = df["lowest_taxonomic_unit_found"]
     return jsonify(otu_descriptions.tolist())
 
 @app.route("/metadata/<sample>")
 def metadata(sample):
+    # read data into pandas and return jsonified data
     sampleID = sample[3:]
     file_name = os.path.join("datasets","Belly_Button_Biodiversity_Metadata.csv")
     df = pd.read_csv(file_name)
-    # df = pd.read_csv("datasets\\Belly_Button_Biodiversity_Metadata.csv")
     meta_data = df[df["SAMPLEID"] == int(sampleID)][["SAMPLEID","ETHNICITY","GENDER","AGE","BBTYPE","LOCATION"]]
     return jsonify(meta_data.to_dict("records"))
 
 
 @app.route("/wfreq/<sample>")
 def washFrequency(sample):
+    # read data into pandas and return jsonified data
     sampleID = sample[3:]
     file_name = os.path.join("datasets","Belly_Button_Biodiversity_Metadata.csv")
     df = pd.read_csv(file_name)
-    # df = pd.read_csv("datasets\\Belly_Button_Biodiversity_Metadata.csv")
     wash_data = df[df["SAMPLEID"] == int(sampleID)]["WFREQ"].astype(int)
     return jsonify(wash_data.tolist())
 
 @app.route("/samples/<sample>")
 def samples(sample):
+    # read data into pandas and return jsonified data
     file_name1 = os.path.join("datasets","belly_button_biodiversity_samples.csv")
     df_samples = pd.read_csv(file_name1)
     
     file_name2 = os.path.join("datasets","belly_button_biodiversity_otu_id.csv")
     df_otu = pd.read_csv(file_name2)
-    # df_samples = pd.read_csv("datasets\\belly_button_biodiversity_samples.csv")
-    # df_otu = pd.read_csv("datasets\\belly_button_biodiversity_otu_id.csv")
+    
     df = df_samples.merge(df_otu,how="left")
     df = df[df[sample] > 0]
+    # sort data by sample value, descending
     samples = df[["otu_id","lowest_taxonomic_unit_found",sample]].sort_values(sample,ascending=False)
     samples.columns = ["otu_id","otu_descriptions","sampleValue"]
     samples = [samples.to_dict("list")]
@@ -77,8 +73,6 @@ def samples(sample):
 
 if __name__ == '__main__':
 
-    #app.run(debug=True)
-    # Bind to PORT if defined, otherwise default to 5000.
     env_port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=env_port)
+    app.run(host='0.0.0.0', port=env_port, debug=True)
 
---
+++ static/index.js
@@ -0,0 +1,201 @@
+/*populute selection options*/
+var name_url = "/names";
+/*get sample names from route "names"*/
+Plotly.d3.json(name_url, function(error, data) {
+    if (error) return console.warn(error);
+    names = Object.values(data);
+    /*add list options*/
+    for (var n = 0; n < names.length; n++) {
+        var $option = document.createElement("option");
+        var $selection = document.querySelector("#selSample")
+        $option.value = names[n];
+        $option.innerHTML = names[n];
+        $selection.appendChild($option);
+    };
+})
+
+/*show metadata*/
+var default_selection = "BB_940";
+
+show_metadata(default_selection);
+
+function show_metadata (route) {
+	/*get metadata from route "metadata/sample name"*/
+	Plotly.d3.json(`/metadata/${route}`, function(error, data) {
+        if (error) return console.warn(error);
+        /*insert row and cells under table to display metadata*/
+        var $tbody = document.querySelector("#metadata");
+        $tbody.innerHTML="";
+        for (k in data[0]) {
+			var $tr = document.createElement("tr");
+			var $td = document.createElement("td");
+			$td.innerText = k + ": " + data[0][k];
+        	$tr.appendChild($td);
+        	$tbody.appendChild($tr);
+        };
+        /*define table cell styles*/
+        var $tds = document.querySelectorAll("td");
+        for (var i=0;i<$tds.length;i++) {
+        	$tds[i].style.paddingLeft="30px";
+        	$tds[i].style.paddingBottom="5px";
+        }
+    });
+}
+
+/*Plot pie chart and bubble chart*/
+/*get sample data from route "samples/sample name"*/
+Plotly.d3.json(`/samples/${default_selection}`, function(error, data) {
+    if (error) return console.warn(error);
+    var sample_value = data[0].sampleValue;
+    var otuID = data[0].otu_id
+    var otuDescription = data[0].otu_descriptions
+    /*select first top 10 values*/
+    var sample_top10 = sample_value.slice(0,10);
+    var otuID_top10 = otuID.slice(0,10);
+    var otuDescription_top10 = otuDescription.slice(0,10);
+    /*define pie chart data*/
+    var piedata = [{
+        labels: otuID_top10,
+        values: sample_top10,
+        type: "pie",
+        hovertext:otuDescription_top10
+    }];
+    /*define pie chart layout*/
+    var layout_pie = {
+      title: "<b>Top 10 Samples by OTU ID</b>",
+      height: 400,
+      width: 500,
+      margin: {
+	    l: 50,
+	    r: 0,
+	    b: 50,
+	    t: 50
+	  },
+    };
+	Plotly.newPlot("pieChart",piedata,layout_pie);
+	/*define bubble chart data*/
+	var bubbledata = [{
+	  x: otuID,
+	  y: sample_value,
+	  text: otuDescription,
+	  mode: 'markers',
+	  marker: {
+	    size: sample_value,
+	    color: otuID,
+	    colorscale:"Earth",
+	  }
+	}];
+	/*define bubble chart layout*/
+	var layout_bubble = {
+      title: "<b>Sample Values vs. OTU ID</b>",
+      height: 600,
+      width: 1500
+    };
+	Plotly.newPlot("bubbleChart",bubbledata,layout_bubble);
+});  
+	
+/*create gauge chart*/
+draw_gaugeChart(default_selection);
+
+function draw_gaugeChart(route) {
+	/*get weekly scrub frequency data from route "wfreq/sample name"*/
+	Plotly.d3.json(`/wfreq/${route}`, function(error, data) {
+	if (error) return console.warn(error);
+	/*define x and y position of pointer tip*/
+	var degrees = (9-data[0])*20,
+	     radius = .5;
+	var radians = degrees * Math.PI / 180;
+	var x = radius * Math.cos(radians);
+	var y = radius * Math.sin(radians);
+
+	/*create a triangle to represent a pointer*/
+	var mainPath = 'M .0 -0.025 L .0 0.025 L ',
+	     pathX = String(x),
+	     space = ' ',
+	     pathY = String(y),
+	     pathEnd = ' Z';
+	var path = mainPath.concat(pathX,space,pathY,pathEnd);
+	/*define data for dot (scatter) and pie chart*/
+	var data = [
+		{ type: 'scatter',
+	   	x: [0,], y:[0],
+	    marker: {size: 28, color:'850000'},
+	    showlegend: false,
+	    name: 'scrubs',
+	    text: data[0],
+	    hoverinfo: 'text+name'},
+	  	
+	  	{ values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9,50/9,50/9,50/9, 50],
+	  	rotation: 90,
+	  	text: ['8-9', '7-8', '6-7','5-6', '4-5', '3-4', '2-3',
+	            '1-2', '0-1', ''],
+	  	textinfo: 'text',
+	  	textposition:'inside',
+	  	marker: {colors:['rgba(30,120,30, .5)', 'rgba(55,135,55, .5)','rgba(80,150,80, .5)',
+	  					'rgba(105,165,105, .5)', 'rgba(130,180,130, .5)','rgba(155,195,155, .5)',
+	  					 'rgba(180,210,180, .5)','rgba(205,225,205, .5)', 'rgba(230,240,230, .5)',
+	                         'rgba(255, 255, 255, 0)']},
+	  	hoverinfo: 'none',
+	  	hole: .5,
+	  	type: 'pie',
+	  	showlegend: false}
+		];
+	/*define the layout, shape and path*/
+	var layout = {
+	  		shapes:[{
+	      	type: 'path',
+	      	path: path,
+	      	fillcolor: '850000',
+	      	line: { color: '850000' }
+	    	}],
+	  		title: '<b>Belly Button Washing Frequency</b><br>Scrups per Week',
+		  	height: 600,
+		  	width: 600,
+		  	/*move the zero point to the middle of the pie chart*/
+		  	xaxis: {zeroline:false, showticklabels:false,
+	             showgrid: false, range: [-1, 1]},
+	  		yaxis: {zeroline:false, showticklabels:false,
+	             showgrid: false, range: [-1, 1]}
+			};
+
+	Plotly.newPlot('gaugeChart', data, layout);
+	});
+}
+
+/*update the plot with new data*/
+function updateCharts(route) {
+    Plotly.d3.json(`/samples/${route}`, function(error, newdata) {
+    	if (error) return console.warn(error);
+        var newsample_value = newdata[0].sampleValue;
+        var newotuID = newdata[0].otu_id;
+        var newotuDescription = newdata[0].otu_descriptions;
+        var newsample_top10 = newsample_value.slice(0,10);
+        var newotuID_top10 = newotuID.slice(0,10);
+        var newotuDescription_top10 = newotuDescription.slice(0,10);
+        
+        /*define new data for the pie chart*/
+        var update_pie = {
+        	labels: [newotuID_top10],
+            values: [newsample_top10],
+            hovertext:[newotuDescription_top10]
+        }
+        /*re-plot pie chart*/
+        Plotly.restyle("pieChart", update_pie);
+		/*define new data for the bubble chart*/
+        var update_bubble = {
+		  x: [newotuID],
+		  y: [newsample_value],
+		  text: [newotuDescription],
+		};
+		/*re-plot bubble chart*/
+		Plotly.restyle("bubbleChart",update_bubble);
+	});
+
+
+}
+/*Get the new route data whenever the dropdown selection changes and pass on to functions*/
+function getData(route) {
+    show_metadata(route);
+    updateCharts(route);
+    draw_gaugeChart(route);
+}
